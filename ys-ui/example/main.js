0c061738a7d975430a6ce7936ace844861336199
GouLanZi <shiquan125521@163.com>
2021-03-09 10:43:38 +0800

    修改项目中换肤变量问题  整合三方和自己的组件换肤变量

--- ys-ui/example/main.js
+++ ys-ui/example/main.js
@@ -1,23 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-/**
- * 使用本地 （需要放开APP.vue 下的样式引入）
- */
+
+// 引入组件库组件
 import ysUi from "../packages/index";
-
-/**
- * 使用私服上的 测试线上
- */
-// import ysUi from "ys-ui";
-// import "ys-ui/dist/styles/ys-ui.css";
-
-// 换主题实现
+// 测试换主题实现
 import "./MyTheme/theme.less";
 Vue.use(ysUi);
-
-// import ViewUI from "view-design";
-// import "view-design/dist/styles/iview.css";
-// Vue.use(ViewUI);
 
 Vue.config.productionTip = false;
 
